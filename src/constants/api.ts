d40cd0dff3d0b7015dd6f50068a4756268a85e54
Santiago220991 <alexander.cardenas10@outlook.com>
2024-02-15 18:06:51 -0500

    feat: add constants folder

---
+++ src/constants/api.ts
@@ -0,0 +1,6 @@
+const APIURL =
+    "https://v3.football.api-sports.io/standings?league=39&season=2021";
+const HOST = "v3.football.api-sports.io";
+const KEY:string =  (process.env.REACT_APP_KEY as string);
+
+export {APIURL, KEY, HOST};
