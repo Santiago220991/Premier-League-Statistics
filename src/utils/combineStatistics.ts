54e948fb81407d168df7f22481c06f1e3f031333
Santiago220991 <alexander.cardenas10@outlook.com>
2024-02-21 19:50:29 -0500

    feat: add utils folder

---
+++ src/utils/combineStatistics.ts
@@ -0,0 +1,6 @@
+function statisticDifference (scored: number, against: number) {
+    const result = scored - against
+    return result
+}
+
+export default statisticDifference
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as statisticDifference } from './combineStatistics';
