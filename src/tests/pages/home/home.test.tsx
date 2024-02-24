b976f9ae018e82f87e6a235061b8b8c082568912
Santiago220991 <alexander.cardenas10@outlook.com>
2024-02-23 20:43:17 -0500

    fix: component tests

--- src/tests/pages/home/home.test.tsx
+++ src/tests/pages/home/home.test.tsx
@@ -6,20 +6,20 @@ describe("Home ", () => {
         homeComponent.component.expect.toBeShown("home-container");
     });
 
-    // test("should contain a text", () => {
-    //     const homeComponent = HomeActions.renderComponent();
-    //     homeComponent.component.expect.toHasText(
-    //         "home-container",
-    //         "Premier League",
-    //     );
-    // });
-    // test("should contain role tags", () => {
-    //     const homeComponent = HomeActions.renderComponent();
-    //     homeComponent.component.expect.toHasRole("img", 3);
-    // });
+    test("should contain a text", () => {
+        const homeComponent = HomeActions.renderComponent();
+        homeComponent.component.expect.toHasText(
+            "home-container",
+            "Premier League",
+        );
+    });
+    test("should contain role tags", () => {
+        const homeComponent = HomeActions.renderComponent();
+        homeComponent.component.expect.toHasRole("img", 3);
+    });
 
-    // test("should render a searchBar", () => {
-    //     const homeComponent = HomeActions.renderComponent();
-    //     homeComponent.component.expect.toBeShown("searchBar-container");
-    // });
+    test("should render a searchBar", () => {
+        const homeComponent = HomeActions.renderComponent();
+        homeComponent.component.expect.toBeShown("searchBar-container");
+    });
 });
